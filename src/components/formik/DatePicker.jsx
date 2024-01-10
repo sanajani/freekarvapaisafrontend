74b73a6853b42dbab7a39eb842065f7b3e10da54
Sanaullah <webdevfarsi@gmail.com>
2024-01-10 10:44:10 +0430

    package file updated

--- src/components/formik/DatePicker.jsx
+++ src/components/formik/DatePicker.jsx
@@ -1,32 +1,41 @@
-/* eslint-disable react/prop-types */
-import DateView from 'react-datepicker'
-import "react-datepicker/dist/react-datepicker.css";
-import { Field, ErrorMessage } from 'formik';
-import ErrorText from '../ErrorText';
+// /* eslint-disable react/prop-types */
+// // import DateView from 'react-datepicker'
+// // import "react-datepicker/dist/react-datepicker.css";
+// import { Field, ErrorMessage } from 'formik';
+// import ErrorText from '../ErrorText';
 
-const DatePicker = (props) => {
-    const {label, name, ...rest} = props
+// const DatePicker = (props) => {
+//     const {label, name, ...rest} = props
+//   return (
+//     <div className='text-right'>
+//         <Field name={name} className=''>
+//             {
+//                 ({form, field}) => {
+//                     const {setFieldValue} = form
+//                     const {value} = field
+//                     return <DateView 
+//                     className='border-4'
+//                     {...rest}
+//                         id={name} 
+//                         {...field} 
+//                         {...rest} 
+//                         selected={value} onChange={val => setFieldValue(name, val)}
+//                     />
+//                 }
+//             }
+//         </Field>
+//         <label htmlFor={name}>{label}</label>
+//         <ErrorMessage name={name} component={ErrorText}/>
+//     </div>
+//   )
+// }
+
+// export default DatePicker
+
+
+const DatePicker = () => {
   return (
-    <div className='text-right'>
-        <Field name={name} className=''>
-            {
-                ({form, field}) => {
-                    const {setFieldValue} = form
-                    const {value} = field
-                    return <DateView 
-                    className='border-4'
-                    {...rest}
-                        id={name} 
-                        {...field} 
-                        {...rest} 
-                        selected={value} onChange={val => setFieldValue(name, val)}
-                    />
-                }
-            }
-        </Field>
-        <label htmlFor={name}>{label}</label>
-        <ErrorMessage name={name} component={ErrorText}/>
-    </div>
+    <div>DatePicker</div>
   )
 }
 
